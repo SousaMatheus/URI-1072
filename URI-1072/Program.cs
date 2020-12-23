debefe723f5f376a806f65ea9803ee839f29d965
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-12-23 18:09:53 -0300

    criado solução

---
+++ URI-1072/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace URI_1072
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int varIn =0;
+            int varOut =0;
+            int i =0;
+
+            do {
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <=20){
+                    varIn ++ ; 
+                }
+                else {
+                    varOut ++;
+                }                
+                i ++;
+            } while (i <n);
+
+            Console.WriteLine(varIn  + " in");
+            Console.WriteLine(varOut + " out");
+
+        }
+    }
+}
